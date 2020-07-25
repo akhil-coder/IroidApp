3b8d557ed1e9550554e67ec2968ee4f367b0f0d8
akhil-coder <akhil.honor@gmail.com>
2020-07-25 21:09:33 +0530

    Layout Updated.

--- app/src/main/java/com/example/iroidapp/HomeScreenActivity.java
+++ app/src/main/java/com/example/iroidapp/HomeScreenActivity.java
@@ -39,7 +39,7 @@ public class HomeScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_redemption);
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
